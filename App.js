e4a075789f5b93f83ef58d4e1b78fdc559b99285
Ariana Winters <arianaw15@gmail.com>
2022-06-21 11:13:47 -0600

    Updated index.js with ReactDOM and fixed routing

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.use(express.static('public'))
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
-const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+ app.listen(port);
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,28 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import { InputPage } from '../src/docs/views/index'
 
 function App() {
   return (
-    <InputPage/>
-  )
+      <Router basename=''>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/inputpage">Input Page</Link>
+              </li>
+              <li>
+                <Link to="/BoxPage">Box Page</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+        <Route path="/" />
+        <Route path="/inputpage" element={<InputPage />} />
+      </Routes>
+        </div>
+      </Router>
+    )
 }
-
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from "react-router-dom";
-
-// const App: React.FC = () => {
-//   return (
-//       <Router basename=''>
-//         <div>
-//           <nav>
-//             <ul>
-//               <li>
-//                 <Link to="/inputpage">Input Page</Link>
-//               </li>
-//               <li>
-//                 <Link to="/BoxPage">Box Page</Link>
-//               </li>
-//             </ul>
-//           </nav>
-  
-//           {/* A <Routes> looks through its children <Route>s and
-//               renders the first one that matches the current URL. */}
-//           <Routes>
-//             <Route path="/inputpage">
-//               <InputPage />
-//             </Route>
-//             {/* <Route path="/boxpage">
-//               <Box />
-//             </Route> */}
-//             <Route path="/">
-
-//             </Route>
-//           </Routes>
-//         </div>
-//       </Router>
-//     )
-// }
 
 export default App;
