fa74ca3fb1f9b51a5f7e445ca35318622ba30c8a
Ariana Winters <arianaw15@gmail.com>
2022-06-22 13:48:12 -0600

    Added mongoose connect

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const path = require('path')
+// const port = 3000
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+// app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
 
- app.listen(port);
+//  app.listen(port);
---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
+
+ app.listen(port, () => console.log('Server started'));
