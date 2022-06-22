c749c0708e211d714ebae6506d81c75c2f55a6d6
Ariana Winters <arianaw15@gmail.com>
2022-06-22 14:37:32 -0600

    Began creating routes

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
 // app.use(express.static(path.join(__dirname, 'build')));
 
 // app.get('/*', function (req, res) {
